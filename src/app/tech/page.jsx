51dd14edda8e9242e21254c06229ef233d8e0109
DowonKim <kimdowon1404@gmail.com>
2024-05-27 11:33:29 +0900

    Feat : css수정 및 tech내용 추가

--- src/app/tech/page.jsx
+++ src/app/tech/page.jsx
@@ -1,3 +1,5 @@
+import { skills } from "@/lib/skills";
+import Image from "next/image";
 import styles from "./tech.module.css";
 
 export const metadata = {
@@ -6,7 +8,18 @@ export const metadata = {
 };
 
 const TechStackPage = () => {
-  return <div>TechStackPage</div>;
+  return (
+    <div className={styles.container}>
+      {skills.map((skill) => (
+        <div key={skill.name} className={styles.wrapper}>
+          <div className={styles.imageBox} />
+          <div className={styles.images}>
+            <Image src={skill.imageUrl} alt={skill.name} fill className={styles.img} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TechStackPage;
---
+++ src/lib/skills.js
@@ -0,0 +1,92 @@
+export const skills = [
+  {
+    imageUrl:
+      "https://img-resize-cdn.joshmartin.ch/768x0%2Cc3537b9f46b5f6055fbc8b4cd03b6b2cc63fc2eefd3d8cd9f0c9f99a5933e496/https://joshmartin.ch/app/uploads/2017/10/css3.svg",
+    name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Three.js_Icon.svg/1200px-Three.js_Icon.svg.png",
+    name: "Threejs",
+    type: "Frontend",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/18133?s=280&v=4",
+    name: "Git",
+    type: "Version Control",
+  },
+  {
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/1200px-GitHub_Invertocat_Logo.svg.png",
+    name: "GitHub",
+    type: "Version Control",
+  },
+  {
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/800px-HTML5_logo_and_wordmark.svg.png",
+    name: "HTML",
+    type: "Frontend",
+  },
+  {
+    imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZeab83jLxJNlqggbc0T40gUZ9D2VGLUg6cI2BNJ6Fwg&s",
+    name: "JavaScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: "https://static-00.iconduck.com/assets.00/react-router-icon-2048x1116-jfeevj0l.png",
+    name: "React-router-dom",
+    type: "Frontend",
+  },
+  {
+    imageUrl: "https://www.drupal.org/files/project-images/nextjs-icon-dark-background.png",
+    name: "Next.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+    name: "React.js",
+    type: "Frontend",
+  },
+  {
+    imageUrl: "https://blog.kakaocdn.net/dn/14SHN/btsg1tFqmtp/VGLOZLfWKOuFVqGbsqTfnK/img.png",
+    name: "Recoil",
+    type: "State Management",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/317889?s=280&v=4",
+    name: "Sass",
+    type: "Frontend",
+  },
+  {
+    imageUrl: "https://velog.velcdn.com/images/tlsl13/post/da1e4478-6993-430d-930e-d0b0618383e8/image.png",
+    name: "Styled-components",
+    type: "Frontend",
+  },
+  {
+    imageUrl: "https://gdsc-university-of-seoul.github.io/assets/images/post-Tailwind-CSS/Tailwind-icon.png",
+    name: "Tailwind CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl:
+      "https://i.namu.wiki/i/6BCaly_IHOsGCno5SofR4NCvQZQp7JzBSaPrRXivLldaA-Rbuceh1oDMN6LfUuZiScaR2eBK7-sGgB-xae_YWA.webp",
+    name: "TypeScript",
+    type: "Frontend",
+  },
+  {
+    imageUrl: "https://miro.medium.com/v2/resize:fit:600/1*i37IyHf6vnhqWIA9osxU3w.png",
+    name: "Jest",
+    type: "Testing",
+  },
+  {
+    imageUrl: "https://velog.velcdn.com/images/real-bird/post/813b4b51-2bf8-41ec-a5d3-b48643c5f78e/image.png",
+    name: "tanstack-query",
+    type: "State Management",
+  },
+  {
+    imageUrl: "https://blog.kakaocdn.net/dn/04lCT/btrg4K7Od47/2gXoYmK6RKIj2bGFGdeeck/img.png",
+    name: "mongoDB",
+    type: "State Management",
+  },
+];
